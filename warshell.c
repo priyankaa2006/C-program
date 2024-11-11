09b8924c74a449f7216d26c89bd0b852225a5230
priyankaa2006 <priyankaa786sk@gmail.com>
2024-11-11 17:43:41 +0530

    warshell.c

--- warshell.c
+++ warshell.c
@@ -1,22 +1,37 @@
-#include<stdio.h>
-#include<conio.h>int
-a[10][10];
-void main()
-{
-intj,k,n;
-clrscr();
-printf("enter the number of vertice\n");
-scanf("%d",&n);
-printf("enter the adjacency matrix");
-for(i=1;i<=n;i++)
-for(j=1;j<=n;j++)
-scanf("%d",&a[i][j]);
-for(k=1;k<=n;k++)
-for(i=1;i<=n;i++)
-for(j=1;j<=n;j++)
-a[i][j]=a[i][j] || a[i][k] && a[k][j]; printf("\n
-the transitive closure is\n");for(i=1;i<=n;i++)
-{
+#include <stdio.h>
+
+int a[10][10];
+
+void main() {
+    int i, j, k, n;
+
+    // Prompt for the number of vertices
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+
+    // Input the adjacency matrix
+    printf("Enter the adjacency matrix:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Applying Floyd-Warshall algorithm for transitive closure
+    for (k = 1; k <= n; k++) {
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                a[i][j] = a[i][j] || (a[i][k] && a[k][j]);
+            }
+        }
+    }
+
+    // Print the transitive closure
+    printf("\nThe transitive closure is:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
 }
-}
-getch();
